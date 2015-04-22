1f68de9f208bd6a511ffcdb09fd306efbdb5f746
Vijay Balasubramanian <vijayasarathib>
2015-04-22 18:51:05 -0400

    v2

--- bin/wrangling_IterativeParsing .py
+++ bin/wrangling_IterativeParsing .py
@@ -1,11 +1,11 @@
 
 # coding: utf-8
 
-# In[1]:
 
 import xml.etree.ElementTree as ET
 import pprint
 
+#function to parse the data and count the tags
 def count_tags(filename):
         tags = {}
         for (event, node) in ET.iterparse(filename, ['start']):
@@ -22,24 +22,12 @@ def test():
     tags = count_tags(r'c:\Vijay\DS\DW\omaha_nebraska.osm')
     pprint.pprint(tags)
     #assert tags == {'bounds': 1, 'member': 3,'nd': 4,'node': 20,'osm': 1,'relation': 1,'tag': 7,              'way': 1}
-
-    
+    # assert tags used to validate the results
 
 if __name__ == "__main__":
     test()
 
 
-# In[25]:
-
-
-
-
-# In[25]:
-
-
-
-
-# In[ ]:
 
 
 
--- bin/wrangling_preparing_db.py
+++ bin/wrangling_preparing_db.py
@@ -12,17 +12,100 @@ import codecs
 import json
 
 
+"""
+Your task is to wrangle the data and transform the shape of the data
+into the model we mentioned earlier. The output should be a list of dictionaries
+that look like this:
+
+{
+"id": "2406124091",
+"type: "node",
+"visible":"true",
+"created": {
+          "version":"2",
+          "changeset":"17206049",
+          "timestamp":"2013-08-03T16:43:42Z",
+          "user":"linuxUser16",
+          "uid":"1219059"
+        },
+"pos": [41.9757030, -87.6921867],
+"address": {
+          "housenumber": "5157",
+          "postcode": "60625",
+          "street": "North Lincoln Ave"
+        },
+"amenity": "restaurant",
+"cuisine": "mexican",
+"name": "La Cabana De Don Luis",
+"phone": "1 (773)-271-5176"
+}
+
+You have to complete the function 'shape_element'.
+We have provided a function that will parse the map file, and call the function with the element
+as an argument. You should return a dictionary, containing the shaped data for that element.
+We have also provided a way to save the data in a file, so that you could use
+mongoimport later on to import the shaped data into MongoDB. 
+
+Note that in this exercise we do not use the 'update street name' procedures
+you worked on in the previous exercise. If you are using this code in your final
+project, you are strongly encouraged to use the code from previous exercise to 
+update the street names before you save them to JSON. 
+
+In particular the following things should be done:
+- you should process only 2 types of top level tags: "node" and "way"
+- all attributes of "node" and "way" should be turned into regular key/value pairs, except:
+    - attributes in the CREATED array should be added under a key "created"
+    - attributes for latitude and longitude should be added to a "pos" array,
+      for use in geospacial indexing. Make sure the values inside "pos" array are floats
+      and not strings. 
+- if second level tag "k" value contains problematic characters, it should be ignored
+- if second level tag "k" value starts with "addr:", it should be added to a dictionary "address"
+- if second level tag "k" value does not start with "addr:", but contains ":", you can process it
+  same as any other tag.
+- if there is a second ":" that separates the type/direction of a street,
+  the tag should be ignored, for example:
+
+<tag k="addr:housenumber" v="5158"/>
+<tag k="addr:street" v="North Lincoln Avenue"/>
+<tag k="addr:street:name" v="Lincoln"/>
+<tag k="addr:street:prefix" v="North"/>
+<tag k="addr:street:type" v="Avenue"/>
+<tag k="amenity" v="pharmacy"/>
+
+  should be turned into:
+
+{...
+"address": {
+    "housenumber": 5158,
+    "street": "North Lincoln Avenue"
+}
+"amenity": "pharmacy",
+...
+}
+
+- for "way" specifically:
+
+  <nd ref="305896090"/>
+  <nd ref="1719825889"/>
+
+should be turned into
+"node_refs": ["305896090", "1719825889"]
+"""
+
+
+
+#Regular expressions to handle the data formats 
 lower = re.compile(r'^([a-z]|_)*$')
 lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
 problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 
 CREATED = [ "version", "changeset", "timestamp", "user", "uid"]
 
-
+# function to parse the data and validate it
 def shape_element(element):
     node = {}
     if element.tag == "node" or element.tag == "way" :
-        # YOUR CODE HERE
+       
         for key in element.attrib.keys():
             val = element.attrib[key]
             node["type"] = element.tag
@@ -66,11 +149,11 @@ def shape_element(element):
             node["node_refs"] = node_refs
 
         return node      
-        #return node
+      
     else:
         return None
 
-
+#function to write the output in the json format
 def process_map(file_in, pretty = False):
     # You do not need to change this file
     file_out = "{0}.json".format(file_in)
@@ -117,13 +200,6 @@ def test():
 if __name__ == "__main__":
     test()
 
-
-# In[2]:
-
-
-
-
-# In[ ]:
 
 
 
